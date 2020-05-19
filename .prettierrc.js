c863f0cacd17c106eeba969ecaf54ec8f519d5ce
gabrielkf <gabrielkf@gmail.com>
2020-05-19 18:12:37 -0300

    Made List and Create routes

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 60,
+  arrowParens: 'avoid',
+  semi: true,
+};
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,23 +10,53 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+const checkRepo = (req, res, next) => {
+  const { id } = req.params;
+
+  const repoIndex = repositories.findIndex(
+    repo => repo.id === id
+  );
+
+  if (repoIndex < 0) {
+    return res
+      .status(400)
+      .json({ error: 'Repository not found' });
+  }
+
+  req.repoIndex = repoIndex;
+
+  next();
+};
+
+app.get('/repositories', (req, res) => {
+  return res.json(repositories);
+});
+
+app.post('/repositories', (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepo);
+
+  return res.json(newRepo);
+});
+
+app.put('/repositories/:id', (req, res) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.delete('/repositories/:id', (req, res) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
+app.post('/repositories/:id/like', (req, res) => {
   // TODO
 });
 
