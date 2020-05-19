affdde1676f6631cadf687b6ed96bd0cfeeffc32
gabrielkf <gabrielkf@gmail.com>
2020-05-19 19:22:13 -0300

    Passed all tests

--- src/app.js
+++ src/app.js
@@ -48,16 +48,43 @@ app.post('/repositories', (req, res) => {
   return res.json(newRepo);
 });
 
-app.put('/repositories/:id', (req, res) => {
-  // TODO
+app.put('/repositories/:id', checkRepo, (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const index = req.repoIndex;
+
+  const editedRepo = {
+    id: repositories[index].id,
+    title: title || repositories[index].title,
+    url: url || repositories[index].url,
+    techs: techs || repositories[index].techs,
+    likes: repositories[index].likes,
+  };
+
+  repositories[index] = editedRepo;
+
+  return res.json(editedRepo);
 });
 
-app.delete('/repositories/:id', (req, res) => {
-  // TODO
+app.delete('/repositories/:id', checkRepo, (req, res) => {
+  const { repoIndex } = req;
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).send();
 });
 
-app.post('/repositories/:id/like', (req, res) => {
-  // TODO
-});
+app.post(
+  '/repositories/:id/like',
+  checkRepo,
+  (req, res) => {
+    const repository = repositories[req.repoIndex];
+
+    repository.likes += 1;
+
+    repositories[req.repoIndex] = repository;
+
+    return res.json(repository);
+  }
+);
 
 module.exports = app;
